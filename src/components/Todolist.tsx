2323022b1e0304feb79ecbb4e987b0ea901d2338
Anton Krasovski <antohkad21@gmail.com>
2024-09-02 20:28:49 +0300

    Add: added function returns list of tasks

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { TaskType } from '../App';
 import {TodolistHeader} from "./TodolistHeader";
+import {Task} from "./Task";
 
 type TodolistProps = {
     title: string,
     tasks: Array<TaskType>
 }
 
-export const Todolist = ({title}: TodolistProps) => {
+export const Todolist = ({title, tasks}: TodolistProps) => {
+    //Returns a list of tasks
+    const tasksList = tasks.length > 0 ? tasks.map(task => {
+        return <Task taskId={task.id} title={task.title} isDone={task.isDone}/>
+    }) : <div>No tasks</div>
 
     return (
         <div>
@@ -17,9 +22,7 @@ export const Todolist = ({title}: TodolistProps) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                {tasksList}
             </ul>
             <div>
                 <button>All</button>
