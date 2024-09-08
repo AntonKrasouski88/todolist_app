e9b7902d5d5a3cf490efe0565bd97b72a388bf7e
Anton Krasovski <antohkad21@gmail.com>
2024-09-08 12:10:08 +0300

    Add: Added empty task input field error

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -8,21 +8,32 @@ type AddTaskFormProps = {
 export const AddTaskForm = ({addTask}: AddTaskFormProps) => {
     const [taskText, setTaskText] = useState<string>('');
 
+    const [error, setError] = useState<string | null>(null);
+
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setTaskText(e.target.value);
 
-    const onKeyHandler =(e: KeyboardEvent<HTMLInputElement>) =>e.key === 'Enter' && onAddTask();
+    const onKeyHandler =(e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === 'Enter' ? onAddTask() : setError(null)
+    };
 
     const onAddTask = () => {
         if(taskText.trim() && taskText.trim().length  < 30) {
             addTask(taskText.trim());
             setTaskText('')
+        } else {
+            setError('Title is required')
         }
     }
 
     return (
         <div>
-            <input value={taskText} placeholder={"Task name"} onChange={onChangeHandler} onKeyUp ={onKeyHandler}/>
+            <input value={taskText}
+                   placeholder={"Task name"}
+                   className={error ? 'error': ''}
+                   onChange={onChangeHandler}
+                   onKeyUp ={onKeyHandler}/>
             <Button title={"+"} onClick={onAddTask} />
+            {error ? <div className={'error-message'}>{error}</div> : ''}
         </div>
     );
 };
